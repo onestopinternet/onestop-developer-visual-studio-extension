93198bbe9aa330eea8ee9107707c33a52062b897
Brett Morrison <brett@onestop.com>
2014-01-02 20:01:14 -0800

    OSPD-1157: Layout update

--- Onestop Developer/OnestopSettings.xaml.cs
+++ Onestop Developer/OnestopSettings.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace Microsoft.VSModalDialog
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+namespace Microsoft.VSModalDialog
 {
     /// <summary>
     /// Interaction logic for OnestopSettings.xaml
@@ -9,5 +15,28 @@
         {
             InitializeComponent();
         }
+
+        private void AcmeRoot_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AcmeRootBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var o = new OpenFileDialog();
+            o.ShowDialog();
+        }
+
+        private void AcmeRoot_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            AcmeRoot.Text = "Hello World";
+        }
+
+        private void SettingsCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
+// ReSharper restore InconsistentNaming
+// ReSharper restore CheckNamespace
